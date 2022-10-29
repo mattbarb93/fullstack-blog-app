0e863675fc65744277b51e5f46f8068f62596685
Matheus Barbosa <mattbarb93@gmail.com>
2022-10-28 23:57:58 -0400

    navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import "../styles/globals.css";
 import "../configureAmplify";
+import Navbar from "./components/navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Navbar />
+      <div className="py-8 px-16 bg-slate-100">
+        <Component {...pageProps} />;
+      </div>
+    </div>
+  );
 }
 
 export default MyApp;
---
+++ pages/components/navbar.js
@@ -0,0 +1,26 @@
+import Link from "next/Link";
+import React from "react";
+import "../../configureAmplify";
+import { useState, useEffect } from "react";
+
+const Navbar = () => {
+  const [signedUser, setSignedUser] = useState(false);
+
+  return (
+    <nav className="flex justify-center pt-3 pb3 space-x-4 border-b bg-cyan-500 border-gray-300">
+      {[
+        ["Home", "/"],
+        ["Create Post", "/create-post"],
+        ["Profile", "/profile"],
+      ].map(([title, url], index) => (
+        <Link href={url} key={index}>
+          <a className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg0-slage-100 hover:text-slate-900">
+            {title}
+          </a>
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default Navbar;
